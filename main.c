986e3cd5e40df93bce220282f8d82a3611941f99
Thirukumaran Sutharshanan <87304377+Suthar98@users.noreply.github.com>
2023-11-29 22:40:03 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main() {
+    int a,b,c;
+    printf("Enter Number 1:\n");
+    scanf("%d",&a);
+    printf("Enter Number 2:\n");
+    scanf("%d",&b);
+    c = a+b;
+    printf("Sum :%d",c);
+    return 0;
+}
